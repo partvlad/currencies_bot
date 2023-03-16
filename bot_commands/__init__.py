9d8389d3fc1d6a7236d51c953a3b29aa35dd6b91
partvlad <metodologista@yandex.ru>
2023-03-16 20:32:50 +0300

    базовые команды

---
+++ bot_commands/__init__.py
@@ -0,0 +1,8 @@
+"""Инициализатор команд бота"""
+from .courses import courses
+from .help import help_command
+from .mine import sub, unsub
+from .start import start
+
+
+__all__ = ('courses', 'help_command', 'start', 'sub', 'unsub')
